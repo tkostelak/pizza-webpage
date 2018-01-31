6345dfaa213bc1d0c2b0d8742b899682276f35d1
Tyler Kostelak <tkostelak@gmail.com>
2018-01-30 23:37:27 -0800

    prototype function uses branching to determine cost of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Business Logic
 var pizzaOrderDisplayText = ("Here is the pizza you created: ");
+var pizzaToppingsArray = [];
 
-
-function Pizza (pizzaSize, pizzaSauce, pizzaToppings, pizzaPrice) {
+function Pizza (pizzaSize, pizzaSauce, pizzaToppingsArray) {
   this.pizzaSize = pizzaSize;
   this.pizzaSauce = pizzaSauce;
-  this.pizzaToppings = pizzaToppings;
-  this.pizzaPrice = pizzaPrice;
+  this.pizzaToppingsArray = pizzaToppingsArray;
   };
+
 
 //User Interface Logic
 $(document).ready(function() {
@@ -18,19 +18,41 @@ $(document).ready(function() {
       var pizzaSauce = $("#pizzaSauce").val();
       var pizzaToppings = $("#pizzaToppings").val();
 
+      var toppingsToArray = pizzaToppings.push(pizzaToppingsArray);
+
       var pizzaPrice = 8;
 
-          $("#pizzaResultsText").append('<div class="pizzaContainer">' + '<br>' +
+      Pizza.prototype.pizzaCost = function() {
+
+        if  (this.pizzaSize === 'Medium') {
+          pizzaPrice += 3;
+        } else if (this.pizzaSize === 'Large') {
+          pizzaPrice += 3;
+        } else if (this.pizzaSize === 'Extra-large' ) {
+          pizzaPrice += 3;
+        };
+
+        if (this.pizzaToppingsArray.length == 1) {
+          pizzaPrice += 2;
+        } else if (this.pizzaToppingsArray.length == 2) {
+          pizzaPrice += 4;
+        } else if (this.pizzaToppingsArray.length == 3) {
+          pizzaPrice += 6;
+        }  else if (this.pizzaToppingsArray.length == 4 || this.pizzaToppingsArray.length > 4 ) {
+          pizzaPrice += 8;
+        } else {
+
+        } return pizzaPrice;
+
+      };
+
+        $("#pizzaResultsText").append('<div class="pizzaContainer">' + '<br>' +
             pizzaOrderDisplayText + '<br>' + '<br>' +
             '<h3 class="pizzaSize:">' + 'Pizza Size: ' + pizzaSize +
             '<h3 class="pizzaSauce:">' + 'Pizza Sauce: ' + pizzaSauce + '</h3>' +
             '<h3 class="pizzaToppings:">' + 'Pizza Toppings: ' + pizzaToppings + '</h3>' +
-            '<h3 class="pizzaPrice:">' + 'Price: ' + pizzaPrice + '</h3>' +
+            '<h3 class="pizzaPrice:">' + 'Price: ' + '$' + pizzaPrice + '</h3>' +
             '</div>'  );
 
-
-
-
-      console.log(pizzaOrder);
     });
 });
