23aa207c9a6717b95fc05afb89904e49ae2f0b20
Tyler Kostelak <tkostelak@gmail.com>
2018-01-30 21:58:50 -0800

    jquery appends HTML directly to page and CSS class places border around appended pizzas made by the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //Business Logic
-var pizza = [];
+var pizzaOrderDisplayText = ("Here is the pizza you created: ");
+
 
 function Pizza (pizzaSize, pizzaSauce, pizzaToppings, pizzaPrice) {
   this.pizzaSize = pizzaSize;
@@ -8,21 +9,8 @@ function Pizza (pizzaSize, pizzaSauce, pizzaToppings, pizzaPrice) {
   this.pizzaPrice = pizzaPrice;
   };
 
-
-
-
-
-
-
 //User Interface Logic
 $(document).ready(function() {
-  var reset = function() {
-    var pizzaSize = $("#pizzaSize").val();
-    var pizzaSauce = $("#pizzaSauce").val();
-    var pizzaToppings = $("#pizzaToppings").val();
-
-
-  }
 
   $("#submitPizza").click(function(event) {
       event.preventDefault();
@@ -32,25 +20,17 @@ $(document).ready(function() {
 
       var pizzaPrice = 8;
 
-      var pizzaOrderDisplayText = ("Here is the pizza you created");
-
-      Pizza.prototype.addPizza = function(Pizza) {
-      $("#pizzaResultsText").append('<div class="petContainer">' +
-          'pizzaOrderDisplayText' +
-          '<h3 class="pizzaSize">' + pizzaSize +
-          '<h3 class="pizzaSauce">' + pizzaSauce + '</h3>' +
-          '<h3 class="pizzaToppings">' + pizzaToppings + '</h3>' +
-          '<h3 class="pizzaPrice">' + pizzaPrice + '</h4>' +
-          '</div>'  );
+          $("#pizzaResultsText").append('<div class="pizzaContainer">' + '<br>' +
+            pizzaOrderDisplayText + '<br>' + '<br>' +
+            '<h3 class="pizzaSize:">' + 'Pizza Size: ' + pizzaSize +
+            '<h3 class="pizzaSauce:">' + 'Pizza Sauce: ' + pizzaSauce + '</h3>' +
+            '<h3 class="pizzaToppings:">' + 'Pizza Toppings: ' + pizzaToppings + '</h3>' +
+            '<h3 class="pizzaPrice:">' + 'Price: ' + pizzaPrice + '</h3>' +
+            '</div>'  );
 
 
+
+
+      console.log(pizzaOrder);
     });
-  });
-});
-
-
-
-$("#resetPizza").click(reset)
-reset();
-
 });
