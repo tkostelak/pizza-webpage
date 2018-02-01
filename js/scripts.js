96fbff7dcfa820394d35bc770120f250c59143d5
tyler Kostelak <tkostelak@gmail.com>
2018-01-31 16:44:02 -0800

    prototype checks most recent pizza in pizza array and calculates price of pizza that the user creates.

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,25 @@ function Pizza (pizzaSize, pizzaSauce, pizzaToppings) {
   this.pizzaToppings = pizzaToppings;
   };
 
+  Pizza.prototype.pizzaCost = function(pizzaSize, pizzaSauce, pizzaToppings) {
+    var pizzaPrice = 8;
 
-  Pizza.prototype.pizzaCost = function() {
+    if (this.pizzaSize === "Medium") pizzaPrice += 2;
+    if (this.pizzaSize === "Large") pizzaPrice += 4;
+    if (this.pizzaSize === "Extra-large") pizzaPrice +=6;
+
+    if(this.pizzaToppings.length === 1) pizzaPrice += 2;
+    if (this.pizzaToppings.length === 2) pizzaPrice += 4;
+    if (this.pizzaToppings.length === 3) pizzaPrice += 6;
+    if (this.pizzaToppings.length >= 4) pizzaPrice += 8;
+
+    $("#pizzaResultsText").append('<div class="pizzaContainer">' + '<br>' +
+        pizzaOrderDisplayText + '<br>' + '<br>' +
+        '<h3 class="pizzaSize:">' + 'Pizza Size: ' + pizzaSize +
+        '<h3 class="pizzaSauce:">' + 'Pizza Sauce: ' + pizzaSauce + '</h3>' +
+        '<h3 class="pizzaToppings:">' + 'Pizza Toppings: ' + pizzaToppings + '</h3>' +
+        '<h3 class="pizzaPrice:">' + 'Price: ' + '$' + pizzaPrice + '</h3>' +
+        '</div>'  );
 
 };
 //User Interface Logic
@@ -23,17 +40,7 @@ $(document).ready(function() {
       var pizzaToppings = $("#pizzaToppings").val();
 
       pizzaArray.push(new Pizza(pizzaSize, pizzaSauce, pizzaToppings));
-
-      var pizzaPrice = 8;
-
-
-        $("#pizzaResultsText").append('<div class="pizzaContainer">' + '<br>' +
-            pizzaOrderDisplayText + '<br>' + '<br>' +
-            '<h3 class="pizzaSize:">' + 'Pizza Size: ' + pizzaSize +
-            '<h3 class="pizzaSauce:">' + 'Pizza Sauce: ' + pizzaSauce + '</h3>' +
-            '<h3 class="pizzaToppings:">' + 'Pizza Toppings: ' + pizzaToppings + '</h3>' +
-            '<h3 class="pizzaPrice:">' + 'Price: ' + '$' + pizzaPrice + '</h3>' +
-            '</div>'  );
-
-    });
+      var currentPizza = pizzaArray[0];
+      currentPizza.pizzaCost(pizzaSize, pizzaSauce, pizzaToppings);
+  });
 });
